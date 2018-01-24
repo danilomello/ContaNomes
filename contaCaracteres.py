ccf16dea74e13b9c150d32057c097dccd8c0d59e
danilomello <33576498+danilomello@users.noreply.github.com>
2018-01-24 15:52:21 -0200

    Add files via upload

--- contaCaracteres.py
+++ contaCaracteres.py
@@ -1,5 +1,15 @@
 #Este código conta a quantidade de caracteres de um nome
 
-def contarCaracteres(nome):
+def qntLetrasNome(nome):
     qntCaracteres = len(nome)
     print("Seu nome é " + nome + " e possui " + str(qntCaracteres) + " caracteres")
+
+def mostraMaiorNome(listaDeNomes):
+    maiorNome = ""
+    qntCaracteres = 0
+    for i in range(len(listaDeNomes)):
+        if len(listaDeNomes[i]) > qntCaracteres:
+            qntCaracteres = len(listaDeNomes[i])
+            maiorNome = listaDeNomes[i]
+
+    print("O maior nome é " + nome + " e possui " + str(qntCaracteres) + " caracteres")
